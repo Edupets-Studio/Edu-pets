82ec030f94e8c824fda87c7d50d4ad056d1f582e
juangarciap1020-star <juangarciap1020@gmail.com>
2026-08-14 16:09:31 -0500

    Update queries.sql

--- BD/queries.sql
+++ BD/queries.sql
@@ -1,1 +1,3 @@
 SELECT usuario FROM Credenciales WHERE contraseña = 777777
+SELECT usuario, contraseña FROM Credenciales WHERE id = 12
+SELECT id FROM Credenciales WHERE contraseña = 777777 AND usuario = Jorge
