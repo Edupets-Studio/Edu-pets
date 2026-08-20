144e1213f59fee64b4f5759ec82dbf32761344ab
NTH-CANNERY <matiascardonacanas5@gmail.com>
2026-08-20 21:01:38 +0000

    consultas extra

--- BD/queries.sql
+++ BD/queries.sql
@@ -1,3 +1,8 @@
 SELECT usuario FROM Credenciales WHERE contraseña = 777777
 SELECT usuario, contraseña FROM Credenciales WHERE id = 12
 SELECT id FROM Credenciales WHERE contraseña = 777777 AND usuario = Jorge
+SELECT usuario, id FROM Credenciales WHERE id = 67
+SELECT contraseña FROM credenciales WHERE usuario = JUAN ESTEBAN
+SELECT contraseña FROM credenciales WHERE usuario = Jorge
+SELECT contraseña FROM credenciales WHERE usuario = Luis Manuel
+SELECT contraseña FROM credenciales WHERE usuario =
